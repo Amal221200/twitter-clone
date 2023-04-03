5526023582218afa68e968ce504ca81a590c05c6
Amal221200 <amalmurikkoli2000@gmail.com>
2023-04-03 11:33:48 +0530

    Auth UI TweetButton

--- components/layout/SidebarTweetButton.tsx
+++ components/layout/SidebarTweetButton.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link"
 import { FaFeather } from 'react-icons/fa'
-// import { useRouter } from "next/router"
+import { useRouter } from "next/router"
+import useLoginModal from "@/hooks/useLoginModal"
+import { useCallback } from "react"
 
 
 const SidebarTweetButton = () => {
-  // const router = useRouter()
+  const router = useRouter()
+  const loginModal = useLoginModal()
+  
+  const onClick = useCallback(() => {
+    loginModal.onOpen()
+  }, [loginModal])
+
   return (
-    <Link href={'/'}>
+    <div onClick={onClick}>
       <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex justify-center items-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
         <FaFeather size={20} color="white" />
       </div>
       <div className="mt-6 hidden lg:block rounded-full p-4 bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
         <p className="hidden lg:block text-center font-semibold text-white text-[20px]">Tweet</p>
       </div>
-    </Link>
+    </div>
   )
 }
 export default SidebarTweetButton
