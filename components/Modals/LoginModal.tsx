f132e55ea61e5c1b393223eaf5d866aa1903d9ae
Amal221200 <amalmurikkoli2000@gmail.com>
2023-04-03 10:28:05 +0530

    Modal Auth UI

---
+++ components/Modals/LoginModal.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useState } from "react"
+import Input from "../Input"
+import Modal from "../Modal"
+import useLoginModal from "@/hooks/useLoginModal"
+import useRegisterModal from "@/hooks/useRegisterModal"
+
+
+const LoginModal = () => {
+    const loginModal = useLoginModal()
+    const registerModal = useRegisterModal()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onSubmit = useCallback(async () => {
+        try {
+            setIsLoading(true)
+
+            // TODO
+
+
+            loginModal.onClose()
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }, [loginModal])
+
+    const onToggle = useCallback(() => {
+        if (isLoading) return
+        loginModal.onClose()
+        registerModal.onOpen()
+    }, [isLoading, loginModal, registerModal])
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Input type="email" value={email} placeholder="Email" onChange={(event) => setEmail(event.target.value)} disabled={isLoading} />
+
+            <Input type="password" value={password} placeholder="Password" onChange={(event) => setPassword(event.target.value)} disabled={isLoading} />
+        </div>
+    )
+
+
+    const footerContent = (
+        <footer className="text-neutral-400 text-center mt-4">
+            <p>First time using twitter? <span className="text-white cursor-pointer hover:underline" onClick={onToggle}>Create an account</span></p>
+        </footer>
+    )
+    return (
+        <Modal disabled={isLoading} isOpen={loginModal.isOpen} title="Login" actionLabel="Sign In" onClose={loginModal.onClose} onSubmit={onSubmit} body={bodyContent} footer={footerContent} />
+    )
+}
+export default LoginModal
---
+++ components/Modals/RegisterModal.tsx
@@ -0,0 +1,60 @@
+import { useCallback, useState } from "react"
+import Input from "../Input"
+import Modal from "../Modal"
+import useRegisterModal from "@/hooks/useRegisterModal"
+import useLoginModal from "@/hooks/useLoginModal"
+
+
+const RegisterModal = () => {
+    const loginModal = useLoginModal()
+    const registerModal = useRegisterModal()
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onSubmit = useCallback(async () => {
+        try {
+            setIsLoading(true)
+
+            // TODO Register and Login
+
+
+            registerModal.onClose()
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }, [loginModal])
+
+    const onToggle = useCallback(() => {
+        if (isLoading) return
+
+        registerModal.onClose()
+        loginModal.onOpen()
+    }, [registerModal, loginModal, isLoading])
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Input type="email" value={email} placeholder="Email" onChange={(event) => setEmail(event.target.value)} disabled={isLoading} />
+
+            <Input type="text" value={name} placeholder="Name" onChange={(event) => setName(event.target.value)} disabled={isLoading} />
+
+            <Input type="text" value={username} placeholder="Username" onChange={(event) => setUsername(event.target.value)} disabled={isLoading} />
+
+            <Input type="password" value={password} placeholder="Password" onChange={(event) => setPassword(event.target.value)} disabled={isLoading} />
+        </div>
+    )
+
+    const footerContent = (
+        <footer className="text-neutral-400 text-center mt-4">
+            <p>Already have an account? <span className="text-white cursor-pointer hover:underline" onClick={onToggle}> Sign In</span></p>
+        </footer>
+    )
+    return (
+        <Modal disabled={isLoading} isOpen={registerModal.isOpen} title="Create an Account" actionLabel="Register" onClose={registerModal.onClose} onSubmit={onSubmit} body={bodyContent} footer={footerContent} />
+    )
+}
+export default RegisterModal
