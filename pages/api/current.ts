d1121ef0f487564f727c5d500e952ea1546cf6df
Amal221200 <amalmurikkoli2000@gmail.com>
2023-04-05 09:12:54 +0530

    .gitignore

--- pages/api/current.ts
+++ pages/api/current.ts
@@ -14,7 +14,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(200).json(currentUser)
 
     } catch (error) {
-        console.log(error)
+        console.log(error, 'MY_SERVER_ERROR')
         res.status(400).end()
     }
 
